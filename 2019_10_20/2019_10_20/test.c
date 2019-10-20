136bea8092af09c2bec9d53e152f8695c1fcbf14
huangkeke28 <huangkeup@163.com>
2019-10-20 22:28:07 +0800

    代码更新

--- 2019_10_20/2019_10_20/test.c
+++ 2019_10_20/2019_10_20/test.c
@@ -132,72 +132,168 @@
 //#include<stdio.h>
 //#include<Windows.h>
 //#pragma warning(disable:4996)
-//void init(int* arr, int)
+//
+//void printArr(int* arr, int len)
+//{
+//	int i = 0;
+//	for (i = 0; i < len; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//void init(int* arr, int len)
 //{
 //	
 //	int i = 0;
-//	for (i = 0; i <= 5; i++)
+//	for (i = 0; i <= len; i++)
 //	{
-//		scanf("%d", arr[i]);
+//		arr[i] = i;
 //	}
+//	printArr(arr, len);
+//	printf("对数组初始化\n");
 //}
-//void empty(int* arr, int)
+//void empty(int* arr, int len)
 //{
 //	int i = 0;
 //	for (i = 0; i <= 5; i++)
 //	{
 //		arr[i] = 0;
 //	}
+//	printArr(arr, len);
+//	printf("对数组清空\n");
 //
 //}
-//void reverse(int* arr, int)
+//void reverse(int* arr, int len)
 //{
-//
+//	int left = 0;
+//	int right= len - 1;
+//	for (left = 0; left <= right; left++)
+//	{
+//		int temp = arr[right];
+//		arr[right] = arr[left];
+//		arr[left] = temp;
+//	}
+//	printArr(arr, len);
+//	printf("对数组元素的逆置\n");
 //}
+////void printArr(int* arr,int len)
+////{
+////	int i = 0;
+////	for (i = 0; i < len; i++)
+////	{
+////		printf("%d ", arr[i]);
+////	}
+////}
 //int main()
 //{
-//	int arr[5];
-//	init(arr,5);
-//	empty(arr,5);
-//	reverse(arr,5);
+//	int arr[5] = {2,3,4,5,6};
+//	int len = sizeof(arr) / sizeof(arr);
+//	printArr(arr, len);
+//	//init(arr,len);
+//	//empty(arr,len);
+//	//reverse(arr,len);
 //	system("pause");
 //	return 0;
 //}
 
 
 //5.实现一个函数，判断一个数是不是素数。
-#include<stdio.h>
-#include<Windows.h>
-#include<math.h>
-#pragma warning(disable:4996)
-
-int isPrime(int i)
-{
-	int x = 0;
-	for (x = 2; x <= i; x++)
-	{
-		if (i%x == 0)
-		{
-			return 0;
-		}
-	}
-	return 1;
-}
-
-
-int main()
-{
-	int i = 0;
-	scanf("%d", &i);
-	if (isPrime(i))
-	{
-		printf("%d是素数\n", i);
-	}
-	else
-	{
-		printf("%d不是素数\n", i);
-	}
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<math.h>
+//#pragma warning(disable:4996)
+//
+//int isPrime(int i)
+//{
+//	int x = 0;
+//	for (x = 2; x <= i; x++)
+//	{
+//		if (i%x == 0)
+//		{
+//			return 0;
+//		}
+//	}
+//	return 1;
+//}
+//
+//
+//int main()
+//{
+//	int i = 0;
+//	scanf("%d", &i);
+//	if (isPrime(i))
+//	{
+//		printf("%d是素数\n", i);
+//	}
+//	else
+//	{
+//		printf("%d不是素数\n", i);
+//	}
+//	system("pause");
+//	return 0;
+//}
 //通过printf打印 如何使它 带上颜色
+
+//4.
+//创建一个数组，
+//实现函数init（）初始化数组、
+//实现empty（）清空数组、
+//实现reverse（）函数完成数组元素的逆置。
+//要求：自己设计函数的参数，返回值。
+//#include<stdio.h>
+//#include<Windows.h>
+//#include<memory.h>
+//void printfarr(int[], int);
+//void init(int* arr,int sz)
+//{
+//	int j = 0;
+//	for (j = 0; j <=sz; j++)
+//	{
+//		arr[j] = j;
+//	}
+//	printf("初始化数组\n");
+//	printfarr(arr, sz);
+//}
+//
+//void empty(int* arr, int sz)
+//{
+//	memset(arr, 0, sz *sizeof(arr[0]));
+//	printf("清空数组\n");
+//	printfarr(arr, sz);
+//}
+//void reverse(int* arr, int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int temp = arr[left];
+//		arr[left] = arr[right - i];
+//		arr[right - i] = temp;
+//	}
+//	printf("逆置数组");
+//	printfarr(arr, sz);
+//	
+//}
+//void printfarr(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 1, 2, 32, 3, 4, 5, 63, 2, 78, 4 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	empty(arr, sz);
+//	init(arr, sz);
+//	//printfarr(arr, sz);
+//	reverse(arr, sz);
+//	system("pause");
+//	return 0;
+//}
