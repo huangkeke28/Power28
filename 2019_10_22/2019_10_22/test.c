20ff3f8ab3f29bebc3461b30c0a2cc8208854950
huangkeke28 <huangkeup@163.com>
2019-10-23 01:35:09 +0800

    代码增加

--- 2019_10_22/2019_10_22/test.c
+++ 2019_10_22/2019_10_22/test.c
@@ -138,41 +138,240 @@
 //	return 0;
 //}
 
-//1.非递归分别实现求第n个斐波那契数。
+//非递归分别实现求第n个斐波那契数。
+//#include<Windows.h>
+//#include<stdio.h>
+//#pragma warning(disable:4996)
+////1 1 2 3 5
+//int fib(int n)
+//{
+//	int i = 0;
+//	int j = 1;
+//	int x = 1;
+//	int temp = 0;
+//	if (n <= 2)
+//	return 1;
+//	else
+//	{
+//		
+//		for (i = 3; i <= n ; i++)
+//		{
+//			temp = x + j;//2 4
+//			j = x;//1
+//			x= temp;//2
+//
+//		}
+//		return temp;
+//
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = fib(n);
+//	printf("%d", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//编写一个函数实现n^k，使用递归实现
+//#include<Windows.h>
+//#include<stdio.h>
+//#pragma warning(disable:4996)
+//
+//int ciFang(int n, int k)
+//{
+//	if (1==k)
+//		return n;
+//	return n*ciFang(n, k - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	int ret = 0;
+//	scanf("%d %d", &n, &k);
+//	ret = ciFang(n, k);
+//	printf("%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//3. 写一个递归函数DigitSum(n)，输入一个非负整数，返回组成它的数字之和，
+//例如，调用DigitSum(1729)，则应该返回1 + 7 + 2 + 9，它的和是19
+//#include<stdio.h>
+//#include<Windows.h>
+//#pragma warning(disable:4996)
+//
+//int DigitDum(int n) //1729 1 +7 +2 +9
+//{
+//	if (n > 9)    //172 9//17 2//1 7//1
+//		return n % 10 + DigitDum(n / 10);
+//	else
+//		return n % 10;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = DigitDum(n);
+//	printf("%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//4. 编写一个函数 reverse_string(char * string)（递归实现）
+//实现：将参数字符串中的字符反向排列。
+//要求：不能使用C函数库中的字符串操作函数。
+
+//#include<Windows.h>
+//#include<stdio.h>
+//void reverse_string(char* arr)
+//{
+//	if (*arr != '/n')
+//		reverse_string(arr++);
+//	else
+//	return printf("%c\n", *arr);
+//
+//}
+//
+//int main()
+//{
+//	char arr[] = { "Hello World" };
+//	reverse_string(arr);
+//	system("pause");
+//	return 0;
+//}
+
+//递归和非递归分别实现strlen
+//递归
+//#include<windows.h>
+//#include<stdio.h>
+//
+//int Strlen(char* arr)
+//{
+//	if (*arr == '\0')
+//		return 0;
+//	else
+//		return 1 + Strlen(arr+1);
+//}
+//
+//int main()
+//{
+//	char arr[] = "asdfgh";
+//	int ret = 0;
+//	ret = Strlen(arr);
+//	printf("%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//非递归
+//#include<stdio.h>
+//#include<Windows.h>
+//
+//int Strlen(char arr[])
+//{
+//	int x = 0;
+//	while (*arr != '\0')
+//	{
+//		arr = arr + 1;
+//		x++;
+//	}
+//	return x;
+//}
+//
+//int main()
+//{
+//	char arr[] = { "asdfg" };
+//	int ret = 0;
+//	ret = Strlen(arr);
+//	printf("%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//6.递归和非递归分别实现求n的阶乘
+//递归
+//#include<Windows.h>
+//#include<stdio.h>
+//#pragma warning(disable:4996)
+//
+//int factorial(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n*factorial(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = factorial(n);
+//	printf("%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+
+//非递归
+//#include<stdio.h>
+//#include<Windows.h>
+//#pragma warning(disable:4996)
+//
+//int factorial(int n)
+//{
+//	int x = 1;
+//	while (n > 1)
+//	{
+//		x = x*n;
+//		n--;
+//	}
+//	return x;
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	int ret = 0;
+//	scanf("%d", &n);
+//	ret = factorial(n);
+//	printf("%d", ret);
+//	system("pause");
+//	return 0;
+//
+//}
+
+
+//递归方式实现打印一个整数的每一位
 #include<Windows.h>
 #include<stdio.h>
 #pragma warning(disable:4996)
-//1 1 2 3 5
-int fib(int n)
+
+void Print(int n)//1234  123 4 12 3 1 3 
 {
-	int i = 0;
-	int j = 1;
-	int x = 1;
-	int temp = 0;
-	if (n <= 2)
-	return 1;
-	else
+	if (n > 9)
 	{
-		
-		for (i = 3; i <= n ; i++)
-		{
-			temp = x + j;//2 4
-			j = x;//1
-			j = temp;//2
-
-		}
-		return temp;
-
+		Print(n / 10);
 	}
+	
+	 printf("%d ", n % 10);
 }
+
 
 int main()
 {
 	int n = 0;
-	int ret = 0;
 	scanf("%d", &n);
-	ret = fib(n);
-	printf("%d", ret);
+	Print(n);
 	system("pause");
 	return 0;
 }
